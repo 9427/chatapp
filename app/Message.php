00b9f0cacecb83e7ebf130de6463d3df1b8178c1
vagrant <vagrant@homestead>
2020-02-17 12:06:19 +0000

    Initial commit

---
+++ app/Message.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    /**
+     * Mass assignable fields
+     *
+     * @var array
+     */
+    protected $fillable = ['message'];
+    
+    public function user()
+    {
+	return $this->belongsTo(User::class);    
+    }
+}
